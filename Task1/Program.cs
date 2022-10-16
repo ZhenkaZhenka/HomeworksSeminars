ee85b36540b7b70e6db9d9e39b096453fd89617d
Evgenii <eaturkin@yandex.ru>
2022-10-16 16:11:00 +0500

    I've added a solved third task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,8 +5,8 @@
 // a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Введите два числа для сравнения");
-int a = Convert.ToInt32(Console.ReadLine());//Введите число
-int b = Convert.ToInt32(Console.ReadLine());//Введите число
+int a = Convert.ToInt32(Console.ReadLine());//Введите число(Метод Convert.ToInt32() использую чтоб не проверять число на Null)
+int b = Convert.ToInt32(Console.ReadLine());//Введите число(Метод Convert.ToInt32() использую чтоб не проверять число на Null)
 
 if (a>b) Console.WriteLine($"max = {a}") ;//Сравниваем числа и выводим максимальное число
 else Console.WriteLine($"max = {b}");
