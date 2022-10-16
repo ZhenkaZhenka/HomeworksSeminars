63bd2577453735e6fe20c595db917b4dcbcfcaca
Evgenii <eaturkin@yandex.ru>
2022-10-16 15:58:01 +0500

    First commit with two tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа для сравнения");
+int a = Convert.ToInt32(Console.ReadLine());//Введите число
+int b = Convert.ToInt32(Console.ReadLine());//Введите число
+
+if (a>b) Console.WriteLine($"max = {a}") ;//Сравниваем числа и выводим максимальное число
+else Console.WriteLine($"max = {b}");
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите 3 числа для сравнения");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c) Console.WriteLine($"{a} - наибольшее число из трех");
+else if (b > a && b > c) Console.WriteLine($"{b} - наибольшее число из трех");
+else Console.WriteLine($"{c} - наибольшее число из трех");
