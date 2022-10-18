f3cab569a2108a13635a27fc133704fbf1a9b2eb
Evgenii <eaturkin@yandex.ru>
2022-10-18 20:48:35 +0500

    Second task for the second seminar has been done

--- Lesson 2/Task2/Program.cs
+++ Lesson 2/Task2/Program.cs
@@ -1,5 +1,11 @@
-﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
+// Не использовать строки для расчета.
 
 // 456 -> 46
 // 782 -> 72
 // 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);//Output a random number for information
+Console.WriteLine($"{number/100}{number%10}");// by the {number / 100} i got first digit, by the {number % 10} i got third number.
