050c7a98ff35a09b11223ad8daa8713d79316758
Evgenii <eaturkin@yandex.ru>
2022-10-18 14:00:53 +0500

    Первая задача второго семинара сохранена с ошибкой где-то в валидаторе

---
+++ Lesson 2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int Number(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool Validation (int a)
+{
+    if (a => 100 && a <= 999) return false;
+    else return true;
+}
+
+int number = Number("Введите трезначное число");
+
+if (Validation(number)) Console.WriteLine($"Вторая цифра введенного числа {number % 100 / 10}");
+else Console.WriteLine("Число не трехзначное");
+
+Console.WriteLine(Validation(number));
