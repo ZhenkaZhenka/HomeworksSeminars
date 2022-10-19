491b89e080ac2360e20285f57ae586a650d4bd61
Evgenii <eaturkin@yandex.ru>
2022-10-19 23:18:53 +0500

    The task 3(23) has done

--- Lesson3/Task3/Program.cs
+++ Lesson3/Task3/Program.cs
@@ -3,3 +3,30 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int Number(string message)//input number
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Number("Input number");
+int[] array = new int[number];
+array = SquareOfNumbers(number);
+
+int[] SquareOfNumbers(int number)
+{
+    for(int i=0; i < number; i++)
+    {
+        int temp = i + 1;
+        array[i] = temp * temp * temp;
+    }
+    return array;
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i < array.Length - 1) Console.Write(", ");
+}
