33b99f35801d1195e2417c22337d118316b42844
Evgenii <eaturkin@yandex.ru>
2022-10-22 16:11:28 +0500

    Task2 lesson 4 has been ready

--- Lesson4/Task2/Program.cs
+++ Lesson4/Task2/Program.cs
@@ -3,3 +3,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int number = Number("Input number you want to count summ of digits");
+
+int Number(string message)//input number
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SummOfDigits(int number)
+{
+    int summ = 0;
+    while (number > 0)
+    {
+        summ = summ + number % 10;
+        number = number / 10;
+    }
+    return summ;
+}
+
+int result = SummOfDigits(number);
+Console.WriteLine($"Summ of digits in {number} is {result}");
