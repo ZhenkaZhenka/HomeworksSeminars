d6593bc18a834cb333b0d6dc0fe8fe209cbaeb5a
Evgenii <eaturkin@yandex.ru>
2022-11-07 23:58:31 +0500

    Task1v2 was added

---
+++ Lesson4/Task1v2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+//рекурсией
+
+int numberA = Number("Input number you want to multiply");
+int numberB = Number("Input number what will be power");
+
+int Number(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Multiplicity (int number, int power)
+{
+    Console.WriteLine("1");
+    if (power == 0) return 1;
+    if (power == 1) return number;
+    return number * Multiplicity(number, power - 1);
+}
+
+int raisedNumber = Multiplicity(numberA, numberB);
+Console.WriteLine($"{raisedNumber} is a raised {numberA} to a power {numberB} ");
