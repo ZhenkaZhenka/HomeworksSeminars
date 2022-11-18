3708740b928c245b0c7fecc135a9d471592d0bb2
Evgenii <eaturkin@yandex.ru>
2022-11-18 14:01:44 +0500

    Task 1, task 2 Lesson 7 have been done

---
+++ Lesson 7/Task 1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:f2}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) + new Random().NextDouble();
+        }
+    }
+    return array;
+}
+
+void Execute()
+{
+    int m = ReadData("Input an amount of the rows in the array");
+    int n = ReadData("Input an amount of the columns in the array");
+    PrintArray(CreateArray(m, n));
+}
+
+Execute();
