10b3b102bfcdbe09cb91235fcb537f53a16c0fce
Evgenii <eaturkin@yandex.ru>
2022-11-25 11:22:40 +0500

    All tasks for Lesson 9 has been done

---
+++ Lesson 9/Task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfTheNaturalRow(int m, int n)
+{
+    if (m > n) return 0;
+    return m + SumOfTheNaturalRow(m+1, n);
+
+}
+
+int m = ReadData("input M");
+int n = ReadData("input N > M");
+Console.WriteLine($"Sum of the natural elements in the row from {m} to {n} is {SumOfTheNaturalRow(m,n)}");
---
+++ Lesson 9/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AccermanFunc(int m, int n)
+{
+    if (m == 0 && n > 0) return n+1;
+    if (m > 0 && n == 0) return AccermanFunc(m - 1, 1);
+    return AccermanFunc(m - 1, AccermanFunc(m, n - 1));
+}
+
+int m = ReadData("input m");
+int n = ReadData("input n");
+Console.WriteLine(AccermanFunc(m, n));
